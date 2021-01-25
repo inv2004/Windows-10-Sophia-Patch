889294d18b93a1e2bf4e844045b4e25cced8aff1
inv2004 <inv2004@gmail.com>
2021-01-25 11:04:39 +0300

    more steps

---
+++ Step2.ps1
@@ -0,0 +1,8 @@
+Get-AppxPackage -AllUsers *ACG* | Remove-AppxPackage
+Get-AppxPackage -AllUsers *CandyCrush* | Remove-AppxPackage
+Get-AppxPackage -AllUsers *Facebook* | Remove-AppxPackage
+Get-AppxPackage -AllUsers *Plex* | Remove-AppxPackage
+Get-AppxPackage -AllUsers *Spotify* | Remove-AppxPackage
+Get-AppxPackage -AllUsers *Twitter* | Remove-AppxPackage
+Get-AppxPackage -AllUsers *Viber* | Remove-AppxPackage
+Get-AppxPackage -AllUsers *3d* | Remove-AppxPackage
